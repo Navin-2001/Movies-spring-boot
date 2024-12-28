cdd1cbc924572f291eb11bc0160b6d745c9115c5
Navin Dissanayaka <127285099+Navin-2001@users.noreply.github.com>
2024-12-28 14:55:37 +0530

    create a movie controller , movie Service and update movie repository
    complete this

--- src/main/java/com/example/movies/MovieRepository.java
+++ src/main/java/com/example/movies/MovieRepository.java
@@ -1,12 +1,16 @@
 package com.example.movies;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
 
 
     
